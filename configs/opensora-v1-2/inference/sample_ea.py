162548820c67d6955606be6a7e5b8fd51646c981
ChengYoghurt <1351425316@qq.com>
2025-01-09 09:54:27 +0800

    Trained ea 15 steps

--- configs/opensora-v1-2/inference/sample_ea.py
+++ configs/opensora-v1-2/inference/sample_ea.py
@@ -9,7 +9,7 @@ verbose = 1
 
 save_latent = False
 ref_latent = "./assets/ea/"
-save_dir = "./samples/ea_samples_step20/"
+save_dir = "./samples/ea_samples_step15/"
 seed = 42
 batch_size = 1
 multi_resolution = "STDiT2"
--- search_rl_scheduler.sh
+++ search_rl_scheduler.sh
@@ -1,10 +1,10 @@
 CUDA_VISIBLE_DEVICES=0 \
 python scripts/search_ea.py \
---outdir 'outputs/search_step20_rl_scheduler' \
+--outdir 'outputs/search_step15' \
 --config '/home/yfeng/ygcheng/src/Open-Sora/configs/opensora-v1-2/inference/sample_ea.py' \
 --n_samples 6 \
 --num_sample 1000 \
---time_step 20 \
+--time_step 15 \
 --max_epochs 10 \
 --population_num 50 \
 --mutation_num 25 \
